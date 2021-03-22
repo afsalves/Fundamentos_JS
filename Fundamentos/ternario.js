5ec201e8b5425ed07d244e6047eac02b24f6abb7
Aline Alves <48930961+afsalves@users.noreply.github.com>
2021-03-22 17:23:23 -0300

    Include new concepts

---
+++ Fundamentos/ternario.js
@@ -0,0 +1,3 @@
+const resultado = nota >= 7 ? 'Aprovado' : 'Reprovado' //estrutura: expressao relacional, retorno se verdadeiro, retorno se falso
+console.log(resultado(7.1))
+console.log(resultado(6.7))
