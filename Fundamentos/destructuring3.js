f5477b484f9141b6f25be43b849d8eaae534365f
Aline Alves <48930961+afsalves@users.noreply.github.com>
2021-03-22 14:46:24 -0300

    Updating destructuring concepts

--- Fundamentos/destructuring3.js
+++ Fundamentos/destructuring3.js
@@ -1,4 +1,4 @@
-function rand ({ min = 0, max = 1000 }) {
+function rand ({ min = 0, max = 1000 }) {  //junto com o destructuring é possível utilizar o parametro padrao
     const valor = Math.random() * (max - min) + min
     return Math.floor(valor)
 }
@@ -6,3 +6,5 @@ function rand ({ min = 0, max = 1000 }) {
 const obj = { max: 50, min: 40  }
 conole.loq(rand(obj))
 console.log(rand({ min:955 }))
+console.log(rand({}))
+console.log(rand()) //dara erro por que estamos desetruturando algo undefined ou nulo
